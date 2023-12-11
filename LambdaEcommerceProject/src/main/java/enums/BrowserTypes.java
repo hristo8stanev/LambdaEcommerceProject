e6254624b0298243cf654a4f5f52793cd0069a92
hristo8stanev <120580916+hristo8stanev@users.noreply.github.com>
2023-12-11 18:29:52 +0200

    BrowserTypes class

--- LambdaEcommerceProject/src/main/java/enums/BrowserTypes.java
+++ LambdaEcommerceProject/src/main/java/enums/BrowserTypes.java
@@ -1,4 +1,37 @@
 package enums;
 
-public class BrowserTypes {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public enum BrowserTypes {
+    CHROME,
+    FIREFOX,
+    CHROME_HEADLESS,
+    FIREFOX_HEADLESS,
+    EDGE;
+
+    public static WebDriver setupBrowser(BrowserTypes browserType) {
+        switch (browserType) {
+            case CHROME:
+                return new ChromeDriver();
+            case FIREFOX:
+                return new FirefoxDriver();
+            case CHROME_HEADLESS:
+                ChromeOptions optionChrome = new ChromeOptions();
+                optionChrome.addArguments("--headless=new");
+                return new ChromeDriver(optionChrome);
+            case FIREFOX_HEADLESS:
+                FirefoxOptions optionFirefox = new FirefoxOptions();
+                optionFirefox.addArguments("--headless");
+                return new FirefoxDriver(optionFirefox);
+            case EDGE:
+                return new EdgeDriver();
+        }
+        return null;
+    }
 }
